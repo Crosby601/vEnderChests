f7bf2cb571fb9862d52a676f6b04eb771244c801
Crosby601 <crosby@ghostcartel.pl>
2024-03-06 00:02:49 +0100

    Init command

--- src/main/java/club/vertuli/venderchests/Commands/EnderChest.java
+++ src/main/java/club/vertuli/venderchests/Commands/EnderChest.java
@@ -7,7 +7,10 @@ import org.bukkit.command.CommandSender;
 public class EnderChest implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] strings) {
-
+        if (label.equalsIgnoreCase("enderchest")) {
+            sender.sendMessage("enderchest open");
+            return true;
+        }
         return false;
     }
 }
